addf4a91f3df57f1169be637e9e54521aa3a0be7
Ezgi Kaltalıoğlu <ezgikaltalioglu25@gmail.com>
2024-01-05 02:45:23 +0300

    fix favorite screen

---
+++ HaberApp/UI/tableView/FavoriTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FavoriTableViewCell.swift
+//  HaberApp
+//
+//  Created by Ezgi Kaltalıoğlu on 5.01.2024.
+//
+
+import UIKit
+
+class FavoriTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
